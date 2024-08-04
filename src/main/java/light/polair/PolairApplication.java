df6b92093041ed3ea4411764153bb2f83572070d
Kangchaeyun77 <rkdcodbs77@naver.com>
2024-08-04 21:20:43 +0900

    initial commit

---
+++ src/main/java/light/polair/PolairApplication.java
@@ -0,0 +1,13 @@
+package light.polair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolairApplication.class, args); // 테스트에용 커밋
+	}
+
+}
---
+++ src/test/java/light/polair/PolairApplicationTests.java
@@ -0,0 +1,13 @@
+package light.polair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
